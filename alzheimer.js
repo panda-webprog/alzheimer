77919e721a60ba7d3c79d70d636a1998271c53b2
fatinnabila00 <47946221+fatinnabila00@users.noreply.github.com>
2019-04-08 11:30:41 +0800

    Update alzheimer.js

--- alzheimer.js
+++ alzheimer.js
@@ -52,3 +52,16 @@ $BTN.click(function () {
   // Output the result
   $EXPORT.text(JSON.stringify(data));
 });
+
+// Hidden sidebar
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
